76d75c87a7740c626b9b1dabfed9ebb016f4e606
Nikita Kulikov <n@kulikov.uk>
2025-06-23 13:59:09 +0300

    Add measure decoder

--- src/main/java/ru/lionzxy/ampersand/ExperimentMain.java
+++ src/main/java/ru/lionzxy/ampersand/ExperimentMain.java
@@ -12,10 +12,10 @@ public class ExperimentMain {
         ChartGUI chartGUI = new ChartGUI();
         chartGUI.start();
 
-        double baseline = 4.0;
+        double baseline = 0.442;
         while (true) {
             int iterationNear = RANDOM.nextInt(3000);
-            baseline = baseline + RANDOM.nextDouble() - 0.5;
+            baseline = baseline + RANDOM.nextDouble() * 0.1 - 0.05;
             for (int i = 1; i <= iterationNear; i++) {
                 double newValue;
 
---
+++ src/main/java/ru/lionzxy/ampersand/utils/MeasureDecoder.kt
@@ -0,0 +1,26 @@
+package ru.lionzxy.ampersand.utils
+
+object MeasureDecoder {
+    fun decode(buffer: Array<UByte>): Pair<UInt, UInt>? {
+        if (buffer.size < 3) {
+            return null
+        }
+
+        var first = buffer[0].toUInt()
+        first += (buffer[1] and 15u).toUInt() shl 8
+
+        var second = buffer[1].toUInt() shr 4
+        second += buffer[2].toUInt() shl 4
+
+        return first to second
+    }
+}
+
+fun main() {
+    val pair = MeasureDecoder.decode(arrayOf(19u, 16u, 2u))
+    if (pair == null) {
+        print("Array too small")
+        return
+    }
+    println("${pair.first} ${pair.second}")
+}
